3bf3e620b60a1052858e897e5d7f6c82872c5ee4
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-03-29 14:39:19 -0300

    Requisito 7: Incrementa um botão que remove o filtro

--- src/components/ApliedFilters.jsx
+++ src/components/ApliedFilters.jsx
@@ -2,15 +2,26 @@ import React, { useContext } from 'react';
 import PlanetsContext from '../context/planetsContext';
 
 function ApliedFilters() {
-  const { filters: { selectors: { values } } } = useContext(PlanetsContext);
+  const { filters: { selectors: { values, onChange } } } = useContext(PlanetsContext);
+  console.log(useContext(PlanetsContext));
   console.log(values);
   return (
     <div>
       <ul>
         {
           values.map(({ id, comparison, column, value }) => (
-            <li key={ id }>
-              {`${column} ${comparison} ${value}`}
+            <li key={ id } data-testid="filter">
+              <p>{`${column} ${comparison} ${value}`}</p>
+              <button
+                value={ id }
+                onClick={ ({ target: { value: idToRemove } }) => {
+                  const filteredSelectors = values
+                    .filter(({ id: actualId }) => idToRemove !== actualId);
+                  onChange([...filteredSelectors]);
+                } }
+              >
+                Remover
+              </button>
             </li>
           ))
         }
